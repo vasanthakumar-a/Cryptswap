6f0091f6681600bd918923ace27705bce128da77
vasanth117 <vasanthakumara117@gmail.com>
2022-04-07 19:52:36 +0530

    deploy

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { TransactionProvider } from '../context/TransactionContext'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }) {
+  return(
+      <TransactionProvider>
+      <Component {...pageProps} />
+      </TransactionProvider>
+    )
+}
+
+export default MyApp
